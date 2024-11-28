e92467d046725aae109de241d95988b252f5ca50
10code14 <130129987+10code14@users.noreply.github.com>
2024-11-28 14:42:47 +0900

    SH

--- code/main.py
+++ code/main.py
@@ -3,12 +3,18 @@ todo_list = []
 def load_todo_list():
     if len(todo_list) == 0:
         print("To-Do List 파일이 존재하지 않습니다.")
+    else:
+        count=0
+        print("현재 To-Do List:")
+        for task in todo_list:
+            count += 1
+            print(f"{count}. {task}")
 
 
 def add_task():
-    """
-    새로운 할 일을 입력받아 To-Do List에 추가하고 저장하느 함수
-    """
+    new_task = str(input("새로운 할 일을 입력하세요: "))
+    todo_list.append(new_task)
+
 
 def main():
     print("1. To-Do List 불러오기")
